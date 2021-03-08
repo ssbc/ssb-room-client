d23aa15f5691d1e664281196e4155f93880a8a62
Andre Staltz <andre@staltz.com>
2021-03-08 15:42:19 +0200

    format code in tunnel-plugin.ts

--- src/tunnel-plugin.ts
+++ src/tunnel-plugin.ts
@@ -10,36 +10,6 @@ function hasConnInstalled(ssb: SSB): ssb is SSBWithConn {
   return !!ssb.conn?.connect;
 }
 
-function init(ssb: SSB) {
-  if (!hasConnInstalled(ssb)) {
-    throw new Error('ssb-room-client plugin requires the ssb-conn plugin');
-  }
-
-  const rooms = new Map<FeedId, RoomObserver>();
-
-  ssb.multiserver.transport({
-    name: 'tunnel',
-    create: makeTunnelPlugin(rooms, ssb),
-  });
-
-  return {
-    connect(opts: ConnectOpts) {
-      if (!opts) return ErrorDuplex('opts *must* be provided');
-      debug('received incoming tunnel.connect(%o)', opts);
-      const {target, portal, origin} = opts;
-      if (target === ssb.id && rooms.has(portal)) {
-        debug('connect() will resolve because handler exists');
-        const handler = rooms.get(portal)!.handler;
-        const [ins, outs] = DuplexPair();
-        handler(ins, origin ?? (this as any).id);
-        return outs;
-      } else {
-        return ErrorDuplex(`could not connect to ${target}`);
-      }
-    },
-  };
-}
-
 module.exports = {
   name: 'tunnel',
   version: '1.0.0',
@@ -49,5 +19,33 @@ module.exports = {
   permissions: {
     anonymous: {allow: ['connect']},
   },
-  init,
+  init(ssb: SSB) {
+    if (!hasConnInstalled(ssb)) {
+      throw new Error('ssb-room-client plugin requires the ssb-conn plugin');
+    }
+
+    const rooms = new Map<FeedId, RoomObserver>();
+
+    ssb.multiserver.transport({
+      name: 'tunnel',
+      create: makeTunnelPlugin(rooms, ssb),
+    });
+
+    return {
+      connect(opts: ConnectOpts) {
+        if (!opts) return ErrorDuplex('opts *must* be provided');
+        debug('received incoming tunnel.connect(%o)', opts);
+        const {target, portal, origin} = opts;
+        if (target === ssb.id && rooms.has(portal)) {
+          debug('connect() will resolve because handler exists');
+          const handler = rooms.get(portal)!.handler;
+          const [ins, outs] = DuplexPair();
+          handler(ins, origin ?? (this as any).id);
+          return outs;
+        } else {
+          return ErrorDuplex(`could not connect to ${target}`);
+        }
+      },
+    };
+  },
 };
